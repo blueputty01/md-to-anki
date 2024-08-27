29ffb78eaa0e72dd4ced7a48b5da5ace127a5cc4
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-08-27 10:41:22 -0400

    Update deck consts

--- deckConsts.py
+++ deckConsts.py
@@ -1,11 +1,15 @@
 ROOT = "/Users/alexyang/Documents/obsidian/default/"
 
 DECKS = {
+    "cs::cmsc351": "@2.1/cmsc351",
+    "cs::cmsc330": "@2.1/cmsc330",
+    "math::math241": "@2.1/math241",
+    "math::stat401": "@2.1/stat401",
     "cs::web": "4 long-term/tech/@web",
     "cs::utils": "3 long-term/tech/@utils",
 }
 
-IGNORE_KEYWORDS = ("discussion")
+IGNORE_KEYWORDS = "discussion"
 
 DECKS = {k: ROOT + v for k, v in DECKS.items()}
 
