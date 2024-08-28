20b96882c4977b775813c22b7e66fd172d151bfd
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-08-28 08:39:17 -0400

    infra(deck consts) remove deck consts

--- test.py
+++
@@ -1,7 +0,0 @@
-from main import parse_markdown
-
-if __name__ == "__main__":
-    with open("test.md", "r", encoding="utf-8") as f:
-        content = f.read()
-        test = parse_markdown(content, "temp", "#delete", "root")
-        print(test)
