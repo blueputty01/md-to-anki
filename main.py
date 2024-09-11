1869665132bd84b4b6dcd917cf71016ff9087617
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2024-09-11 15:56:45 -0400

    fix(markdown-parser) Replace markdown parser

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import sys
 import re
 
-import markdown2
+import markdown
+from markdown.extensions import codehilite, fenced_code
+
 from urllib.parse import unquote
 
 from rich.console import Console
@@ -23,32 +25,12 @@ def parse_markdown(content, deck_name, tags, media_root):
             nonlocal BOLD_TAGS
             raw_string = raw_string.strip()
 
-            s = markdown2.markdown(
+            s = markdown.markdown(
                 raw_string,
-                extras={
-                    "breaks": {"on_newline": True, "on_backslash": True},
-                    # Allows a code block to not have to be indented by fencing it with '```' on a line before and after
-                    # Based on http://github.github.com/github-flavored-markdown/ with support for syntax highlighting.
-                    "fenced-code-blocks": None,
-                    # tables: Tables using the same format as GFM and PHP-Markdown Extra.
-                    "tables": None,
-                    # cuddled-lists: Allow lists to be cuddled to the preceding paragraph.
-                    "cuddled-lists": None,
-                    # code-friendly: The code-friendly extra disables the use of leading, trailing and
-                    # --most importantly-- intra-word emphasis (<em>) and strong (<strong>)
-                    # using single or double underscores, respectively.
-                    "code-friendly": None,
-                    # footnotes: support footnotes as in use on daringfireball.net and implemented in other
-                    # Markdown processors (tho not in Markdown.pl v1.0.1).
-                    "footnotes": None,
-                    # smarty-pants: Fancy quote, em-dash and ellipsis handling similar to
-                    # http://daringfireball.net/projects/smartypants/. See old issue 42 for discussion.
-                    "smarty-pants": None,
-                    # target-blank-links: Add target="_blank" to all <a> tags with an href.
-                    # This causes the link to be opened in a new tab upon a click.
-                    "target-blank-links": None,
-                    "middle-word-em": {"allowed": False},
-                }
+                extensions=[
+                    codehilite.CodeHiliteExtension(),
+                    fenced_code.FencedCodeExtension(),
+                ]
             )
             s = s.replace("<p>", "").replace("</p>", "")
             # middle-word-em also ignores bold notation within latex, so we need to manually add that back
--- tests/test.md
+++ tests/test.md
@@ -1,3 +1,6 @@
+A **vector-valued** function consists of two parts: a **domain**, which is a collection of numbers, and a **2::rule**, which assigns to each number in the domain one and only one vector.
+$\mathbf{F}(t)=f_{1}(t)\mathbf{i}+f_{2}(t)\mathbf{j}+f_{3}(t)\mathbf{k}$
+
 Suppose we have two functions $f(x)$ and $g(x)$ such that $\lim_{x \rightarrow \infty}f(x)$ and $\lim_{x \rightarrow \infty}g(x)$  exist (may be $\infty$). Then:
 $**\lim_{x \rightarrow \infty} \frac{f(x)}{g(x)} \neq \infty \implies f(x)** = O(g(x))$
 $**\lim_{x \rightarrow \infty} \frac{f(x)}{g(x)} \neq 0 \implies f(x)** = \Omega(g(x))$
